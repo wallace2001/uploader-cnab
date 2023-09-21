bde5b1a3f8ef33adc75e9c41dd345f7c199beee1
Wallace <dev_kollen@outlook.com>
2023-09-21 09:40:19 -0300

    create project

---
+++ src/main/java/com/wallace/uploadcnab/UploadCnabApplication.java
@@ -0,0 +1,13 @@
+package com.wallace.uploadcnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadCnabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallace/uploadcnab/UploadCnabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace.uploadcnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadCnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
